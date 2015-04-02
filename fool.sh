32bbc5e565c1e5b76fbe0489ec5af1ed8b12dccd
Qiming He <Qiming.He@openresearchinc.com>
2015-04-02 15:25:41 -0400

    update to use top new players

--- fool.sh
+++ fool.sh
@@ -6,12 +6,12 @@ if [ $1 ]; then
 fi 
 thedate=`date -d "$daysago day ago" +%Y-%m-%d`
 
-echo "Player Rank Stock Date Time Price" |awk '{printf "%-20s%-5s%-10s%-15s%-10s%-10s\n",$1,$2,$3,$4,$5,$6,$7}'
-cat fooltopplayers |while read player
+echo "Player Rank Stock Date Time Price" |awk '{printf "%-20s%-10s%-10s%-15s%-10s%-10s\n",$1,$2,$3,$4,$5,$6,$7}'
+cat fooltopnewplayers |while read player
 do
 	rank=`curl "http://api.fool.com/caps/ws/caps/ws/Player/$player/Picks/Active?apikey=$FOOL_API_KEY" |grep PlayerRank |cut -d'>' -f2 |cut -d'<' -f1`
 	curl "http://api.fool.com/caps/ws/caps/ws/Player/$player/Picks/Active?apikey=$FOOL_API_KEY" |egrep -A 2 -B 5 "<StartDate>$thedate"  |egrep "TickerSymbol|StartDate|StartPrice|--" | cut -d'>' -f2 |cut -d'<' -f1 |tr '\n' ' ' |sed "s/--/\n/g" |while read  trans
 	do
-		echo $player $rank $trans |awk '{printf "%-20s%-5s%-10s%-15s%-10s%-10s\n",$1,$2,$3,$4,$5,$6,$7}'
+		echo $player $rank $trans |awk '{printf "%-20s%-10s%-10s%-15s%-10s%-10s\n",$1,$2,$3,$4,$5,$6,$7}'
 	done
 done
