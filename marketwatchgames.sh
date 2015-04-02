fc0d9c0851ae72f372a3a78c35a5da54d4343445
Qiming He <Qiming.He@openresearchinc.com>
2015-04-02 15:12:45 -0400

    replace old 2014 with new 2015 my favoriate wxc games

--- marketwatchgames.sh
+++ marketwatchgames.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-for game in "practice-stocks-for-fun" "redditchallenge2014" "%E5%A4%A7%E5%8D%83%E5%8D%8A%E5%B9%B4%E6%A8%A1%E6%8B%9F%E8%B5%9B" "%E5%A4%A7%E5%8D%83%E5%8D%8A%E5%B9%B4%E7%82%92%E8%82%A1%E6%AF%94%E8%B5%9B"
+#hand pick stock trading games from marketwatch 30K games
+export wxcweekly=`curl "http://www.marketwatch.com/game/find?sort=CreateDate&descending=True&difficulty=all&search=wxc" |egrep -o 'wxc-dq-weekly-game-[0-9]+' |head -n 1`
+export wxcmonthly=`curl "http://www.marketwatch.com/game/find?sort=CreateDate&descending=True&difficulty=all&search=wxc" |egrep -o 'wxc[a-z\-]+2015-game' |head -n 1`
+
+for game in $wxcweekly $wxcmonthly
 do #marketwatch.com stock trading games/contest of interest
 echo "Player Rank Stock Date Action Shares Price" |awk '{printf"%-30s %-5s %-10s %-10s %-10s %-10s %-10s\n",$1,$2,$3,$4,$5,$6,$7}' >$game.log
-	for page in 0 10 20 30 40 50 60 70 80 90 
+	for page in 0 10 20 
 	do #Top 100 players
 		rank=$page
 		curl "http://www.marketwatch.com/game/$game/ranking?index=$page" |egrep "/game/$game/portfolio/holdings" |cut -d'>' -f2 |cut -d'"' -f2 |cut -d'=' -f2- |sed -e 's/amp//g' -e 's/;//g' |while read url
@@ -12,11 +16,9 @@ echo "Player Rank Stock Date Action Shares Price" |awk '{printf"%-30s %-5s %-10s
 			rank=`expr $rank + 1`		
 			curl "http://www.marketwatch.com/game/$game/portfolio/transactionhistory?name=$url" |egrep -A 12 "/investing/stock/" |grep -v numeric  |sed -e 's/<td>//g'  -e 's/<\/td>//g'  -e 's/<a href="\/investing\/stock\///g' -e 's/<\/a>//g' -e 's/^[ \t]*//g'  -e '/^$/d' |cut -d'"' -f1 |dos2unix |tr '\n' ' ' |sed -e 's/\-\-/\n/g' |sed "s/<span title='InsufficientBuyingPower'>(Canceled)<\/span>//g" |awk '{print $0}' |while read trans
 			do			
-				echo $name $rank $trans |sed 's/\%20/-/g' |awk '{printf"%-30s %-5s %-10s %-10s %-10s %-10s %-10s\n",$1,$2,$3,$4,$8,$9,$10}' >> $game.log
+				echo $name $rank $trans |egrep -v '<span>' |sed 's/\%20/-/g' |awk '{printf"%-30s %-5s %-10s %-10s %-10s %-10s %-10s\n",$1,$2,$3,$4,$8,$9,$10}' >> $game.log
 			done			
 		done	
 	done
 done
 
-mv "%E5%A4%A7%E5%8D%83%E5%8D%8A%E5%B9%B4%E6%A8%A1%E6%8B%9F%E8%B5%9B.log" 		daqian1.log
-mv "%E5%A4%A7%E5%8D%83%E5%8D%8A%E5%B9%B4%E7%82%92%E8%82%A1%E6%AF%94%E8%B5%9B.log"	daqian2.log
