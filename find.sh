cfea351d91e03c5ce3a0622524bebc19ea8d5a13
Qiming He <Qiming.He@openresearchinc.com>
2015-04-09 09:20:53 -0400

    find common transaction from top players

---
+++ find.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Find intersection of all traders' transaction given $days ago
+if [ $# -ne 1 ]; then #if no argument assume yesterday
+    export thedate=` date -d "1 day ago" +/%m/%d/%Y| sed 's/\/0/\/(0)?/g' |sed 's/\/20/\/(20)?/g' |cut -c2-`
+else #otherwise 2,3,4.. days ago 
+    export thedate=`date -d "$1 day ago" +/%m/%d/%Y| sed 's/\/0/\/(0)?/g' |sed 's/\/20/\/(20)?/g' |cut -c2- `
+fi
+
+cat *.log |egrep "$thedate" #find common transactions from fetch.sh
