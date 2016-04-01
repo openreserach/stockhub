66794800ee3076f6876d9f3bda674c06688734f0
QimingHe <qiminghe@hotmail.com>
2016-04-01 11:25:21 -0400

    fix new url

--- seekingalphalong.sh
+++ seekingalphalong.sh
@@ -1,2 +1,2 @@
 
-curl "http://seekingalpha.com/analysis/investing-ideas/long-ideas" |grep bull |egrep -o "\/symbol\/[a-zA-Z0-9\-\.]+"  |cut -d'/' -f3 |tr '[:lower:]' '[:upper:]'
+curl "http://seekingalpha.com/stock-ideas/long-ideas" |grep bull |egrep -o "\/symbol\/[a-zA-Z0-9\-\.]+"  |cut -d'/' -f3 |tr '[:lower:]' '[:upper:]'
