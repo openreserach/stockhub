db86599cfe5ac595a23a21fbffe0b0c6589863f1
QimingHe <qiminghe@hotmail.com>
2017-01-09 14:55:34 -0500

    exclude time-consuming marketwatch games pull from daily job

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-\rm *.log
+\rm -f covester.log fool.log marketocracy.log thelion.log
 ./covestor.sh > covestor.log
-./marketwatchgames.sh
 ./fool.sh 
+./marketocracy.sh
 
 thisyear=`date +%Y`
 lastyear=`echo $thisyear - 1 | bc`
 ./thelion.sh |egrep -v "Closed" |egrep "$thisyear|$lastyear" > thelion.log
+#./marketwatchgames.sh #take long to complete. do it once a while 
