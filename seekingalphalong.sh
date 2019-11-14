a7be1e3367b53acbc40dd485655dcc3fde56a6a8
Qiming He <Qiming.He@openresearchinc.com>
2019-11-14 20:41:06 +0000

    refresh curl over https and adapt HTML page changes

--- seekingalphalong.sh
+++ seekingalphalong.sh
@@ -1,2 +1,3 @@
 
-curl "http://seekingalpha.com/stock-ideas/long-ideas" |grep bull |egrep -o "\/symbol\/[a-zA-Z0-9\-\.]+"  |cut -d'/' -f3 |tr '[:lower:]' '[:upper:]'
+mycurl="curl -stderr -L -k -A 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1'"
+$mycurl "https://seekingalpha.com/stock-ideas/long-ideas" |grep bull |egrep -o "\/symbol\/[a-zA-Z0-9\-\.]+"  |cut -d'/' -f3 |tr '[:lower:]' '[:upper:]' > seekingalphalong.csv
