9350e2d1e173370d656abeed36fbcc97e6458c2a
Qiming He <Qiming.He@openresearchinc.com>
2019-11-15 22:32:18 +0000

    refresh with new sources

--- fetch.sh
+++ fetch.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-\rm -f covester.log fool.log marketocracy.log thelion.log
-./covestor.sh > covestor.log
-./fool.sh 
-./marketocracy.sh
-
-thisyear=`date +%Y`
-lastyear=`echo $thisyear - 1 | bc`
-./thelion.sh |egrep -v "Closed" |egrep "$thisyear|$lastyear" > thelion.log
-#./marketwatchgames.sh #take long to complete. do it once a while 
+./fool.sh
+./seekingalphalong.sh
+./marketwatchgames.sh
