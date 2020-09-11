8712c161d88f0010111e5d890a5d3f3cbf372aed
Qiming He <qiming.he@openresearchinc.com>
2020-09-11 12:57:17 +0000

    add earning calendar; recount common selections

--- rating.sh
+++ rating.sh
@@ -11,10 +11,10 @@ price=$(cat tmp|grep "Current stock price" |cut -d'>' -f5- |cut -d'<' -f1)
 updown=$(cat tmp |egrep 'Change</td>' |egrep -o "color.+"|cut -d'>' -f2 |cut -d'<' -f1)
 echo "$"$price $updown `date +%x`
 echo "FA color-coded=============================="
-for key in 'Market Cap' 'P/E' 'Forward P/E' 'P/S' 'P/C' 'P/FCF' 'P/B' 'Debt/Eq' 'Current Ratio' 'ROA' 'ROE' 'EPS next 5Y' 'Dividend %'
+for key in 'Market Cap' 'P/E' 'Forward P/E' 'P/S' 'P/C' 'P/FCF' 'P/B' 'Debt/Eq' 'Current Ratio' 'ROA' 'ROE' 'EPS next 5Y' 'Dividend %' 'Earnings'
 do
 	color=`cat tmp |grep ">$key<" |egrep -o "color:#[0-9a]+" |cut -d':' -f2`
-	val=`cat tmp |grep ">$key<" |egrep -o ">[0-9]+.[0-9]+<|>[0-9]+.[0-9]+B<|>[0-9]+.[0-9]+M<|>[0-9]+.[0-9]+%<" |sed -e 's/>//g' -e 's/<//g'`
+	val=`cat tmp |grep ">$key<" |egrep -o ">[0-9]+.[0-9]+<|>[0-9]+.[0-9]+B<|>[0-9]+.[0-9]+M<|>[0-9]+.[0-9]+%<|[A-Z][a-z]+ [0-9]+" |sed -e 's/>//g' -e 's/<//g'`
 	if [ "$color" == '#aa0000' ]; then 
 		echo -e "$key:\t\t\e[00;31m$val\e[00m" 
 	elif [ "$color" == '#008800' ]; then 
@@ -135,7 +135,7 @@ $mycurl "https://seekingalpha.com/stock-ideas/long-ideas"  |grep bull |egrep -o 
 
 #Whale Wisdom
 echo "Buy---#Recent filer---------------------------------------------------------"
-egrep "^$1,"  whalewisdom*.csv
+egrep "^$1,"  whalewisdom*.csv |sort | uniq
 
-gurufocus=$(egrep -w $1 gurufocus.csv)
-[[ ! -z $gurufocus ]] && echo "GuruFocus Latest Buy"
+#Gurufocus Latest Buy
+egrep -w $1 gurufocus.csv |while read guru; do echo "GuruFocus Latest Buy:"$guru; done
